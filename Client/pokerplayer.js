82913430a0df3350437584d17b302d2ed1b86c78
mmcshea <62720792+mmcshea@users.noreply.github.com>
2020-05-06 22:16:35 -0400

    just testing

--- Client/pokerplayer.js
+++ Client/pokerplayer.js
@@ -382,21 +382,21 @@ function cl_show_board() {
 
 function cl_deal_flop() {
   cl_write_all_players();
-  gui_burn_board_card(0, "blinded");
-  setTimeout(gui_lay_board_card, 500, 0, LOCAL_STATE.board[0]);
-  setTimeout(gui_lay_board_card, 1000, 1, LOCAL_STATE.board[1]);
-  setTimeout(gui_lay_board_card, 1500, 2, LOCAL_STATE.board[2]);
+  setTimeout(gui_burn_board_card, 500, 0, "blinded");
+  setTimeout(gui_lay_board_card, 1000, 0, LOCAL_STATE.board[0]);
+  setTimeout(gui_lay_board_card, 1500, 1, LOCAL_STATE.board[1]);
+  setTimeout(gui_lay_board_card, 2000, 2, LOCAL_STATE.board[2]);
 }
 
 function cl_deal_fourth() {
   cl_write_all_players();
-  gui_burn_board_card(1, "blinded");
-  setTimeout(gui_lay_board_card, 500, 3, LOCAL_STATE.board[3]);}
+  setTimeout(gui_burn_board_card, 500, 1, "blinded");
+  setTimeout(gui_lay_board_card, 1000, 3, LOCAL_STATE.board[3]);}
 
 function cl_deal_fifth() {
   cl_write_all_players();
-  gui_burn_board_card(2, "blinded");
-  setTimeout(gui_lay_board_card, 500, 4, LOCAL_STATE.board[4]);
+  setTimeout(gui_burn_board_card, 500, 2, "blinded");
+  setTimeout(gui_lay_board_card, 1000, 4, LOCAL_STATE.board[4]);
 }
 
 function cl_get_my_seat() {
